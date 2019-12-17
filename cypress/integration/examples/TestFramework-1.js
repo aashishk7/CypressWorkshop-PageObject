ce886fe58aa44176f4be462fd8c6a2a726d44080
Aashish <aashish.khetarpal@external.mckinsey.com>
2019-12-17 11:15:36 +0530

    code commit

--- cypress/integration/examples/TestFramework-1.js
+++ cypress/integration/examples/TestFramework-1.js
@@ -6,7 +6,7 @@ import CheckoutPage from '../../support/PageObjects/CheckoutPage';
 import BillingPage from '../../support/PageObjects/BillingPage'
 
 describe('Automation Test Suite ', function() {
-    
+
     //Mostly used for Setup Part
     before(function(){
     cy.fixture('example').then(function(data)
@@ -14,26 +14,26 @@ describe('Automation Test Suite ', function() {
         this.data=data ;
     })
     })
-    
+
     it('My First Test Case', function() {
     //Object Creation for PageObject Page Class and assigning it to a constant variable
 
     const homePage=new HomePage();
     const checkoutPage= new CheckoutPage();
     const billingPage=new BillingPage();
-    
+
     //Calling
     cy.visit(Cypress.env('url'));
     homePage.getUserName().type(this.data.Username);
     homePage.getEmail().type(this.data.Email);
     homePage.getPassword().type(this.data.Password)
-    
+
     // Checking whether the Register button is disabled or not either by grabbing the property or by checking its UI behavior
 
     //homePage.getRegisterButton().should('have.attr','disabled','disabled');
     //homePage.getRegisterButton().should('be.disabled');
-    // Clicking on to register on the Website by entering new password 
-    
+    // Clicking on to register on the Website by entering new password
+
     //Pause Command
     //cy.pause();
 
@@ -43,6 +43,8 @@ describe('Automation Test Suite ', function() {
     //Checking whether the Registration is successful and whether UserName is populated under login section
     homePage.getLoginUserName().should('have.value',this.data.Username);
 
+
+    //Using Custom Commands
     // For Loop for Accessing productName array from Features File
     this.data.productName.forEach(function(element){
         cy.selectProduct(element[0],element[1],element[2]);
@@ -66,7 +68,7 @@ describe('Automation Test Suite ', function() {
     billingPage.getBillingFirstName().clear().type(this.data.BillingFirstName);
     billingPage.getBillingLastName().clear().type(this.data.BillingLastName);
     billingPage.getBillingAddress().clear().type(this.data.StreetAddress);
-    
+
     //Changing the timeout from 4 seconds to 10 seconds
     Cypress.config('defaultCommandTimeout',10000)
     //
@@ -87,7 +89,7 @@ describe('Automation Test Suite ', function() {
     billingPage.getOrderPlacedText().then(function(element){
         expect(element.text().includes("Thank you")).to.be.true;
     })
-    
+
     //Mostly Used for TearDown Part
     after(function(){
     })
